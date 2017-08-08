cb7e47ced860ae4ea6d00456af4d48278570de46
Glen Young <extractofcactus@icloud.com>
2017-08-08 16:31:45 +0100

    ReviewView file refactored

---
+++ client/src/views/quoteView.js
@@ -0,0 +1,26 @@
+var ReviewView = function(reviews){
+  this.render(reviews);
+}
+
+ReviewView.prototype = {
+  render: function(reviews){
+    
+    console.log(reviews);
+    reviews.forEach( function(review){
+      var liName = document.createElement('li');
+      var liTitle = document.createElement('li');
+      var li = document.createElement('li');
+      var text = document.createElement('p');
+      var ul = document.getElementById('reviews');
+      liName.innerText = review.performer;
+      liTitle.innerText = review.showTitle;
+      text.innerText = review.review;
+      li.appendChild(text);
+      ul.appendChild(liName);
+      ul.appendChild(liTitle);
+      ul.appendChild(li);
+    });
+  }
+}
+
+ module.exports = ReviewView;
